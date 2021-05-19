271eee8fa36c4cc1c15aacbfa141818d27be7311
Sam Ruben Abraham <63494859+Coder-X15@users.noreply.github.com>
2021-05-19 12:05:04 +0530

    Add files via upload

--- MCUOS.h
+++ MCUOS.h
@@ -15,11 +15,10 @@ class OperatingSystem
 			lcd_display.initialize_display();  //initializing display 
 			sub_scheduler.add_chunk(button_read, button_read.priority);
 			sub_scheduler.add_chunk(button_check, button_check.priority);
-			app_tray.add_app(launcher, launcher.priority); //adding the launcher to the app tray
+			app_tray.add_app(launcher, launcher.priority, launcher.name); //adding the launcher to the app tray
 		}
 		void run(){
-			app_tray.load_app(0); //loading the launcher
-			app_tray.run_app(0);  //running it
+			home(); //loading the launcher andrunning it
 		}
 }os;
 
